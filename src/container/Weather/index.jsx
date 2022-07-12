5e33eaa26f45f81059584141fa1b890f446ec14a
Reza Babakhani <itsreza@Rezas-Mac-mini.local>
2022-07-12 23:47:24 +0430

    start working on forecast weather

--- src/container/Weather/index.jsx
+++ src/container/Weather/index.jsx
@@ -1,10 +1,11 @@
 import moment from "moment";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getRequestMethod } from "../../services/httpService";
 import classes from "./index.module.scss";
 
 export default function Weather() {
+  const [weather , setWeather] = useState()
   const searchParam = useLocation()?.search;
   const dateURLQueryString = new URLSearchParams(searchParam).get("date");
 
@@ -18,13 +19,22 @@ export default function Weather() {
     // const apiAddress =  `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=35.7219&lon=51.3347&dt=${convertedDateToTimeStamp}&appid=${process.env.REACT_APP_WEATHER_SERVICE_TOKEN}`
     // const apiAddress =  `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=35.7219&lon=51.3347&dt=${convertedDateToTimeStamp}&appid=${process.env.REACT_APP_WEATHER_SERVICE_TOKEN}`
     const apiAddress = `https://api.openweathermap.org/data/2.5/weather?lat=35.7219&lon=51.3347&dt=${convertedDateToTimeStamp}&appid=${process.env.REACT_APP_WEATHER_SERVICE_TOKEN}`;
-    getRequestMethod(apiAddress).then((response) => console.log("W", response));
+    getRequestMethod(apiAddress).then((response) => setWeather(response?.data));
   }, []);
 
   return (
     <div>
       <div className={classes.title}>⛅️ Forecast</div>
       <div className={classes.date}>Date : {showingDate}</div>
+      <div className={classes.date}>
+      {weather?.name}
+      </div>
+      <div>Weather</div>
+      <img src={`http://openweathermap.org/img/wn/${weather?.weather?.[0]?.icon}`} alt="" />
+      {console.log(weather?.weather?.[0]?.icon)}
+      <div>Wind</div>
+      <div>Speed {weather?.wind.speed}</div>
+      <div>Degree {weather?.wind.deg}</div>
       {/* {convertedDateToTimeStamp} */}
     </div>
   );
